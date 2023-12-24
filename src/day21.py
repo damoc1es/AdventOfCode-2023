d527a76f6b228eb04b45e0288becb9caf40b1a4b
Damocles <38077049+damoc1es@users.noreply.github.com>
2023-12-24 22:14:24 +0200

    Add day 21 part 2

--- src/day21.py
+++ src/day21.py
@@ -3,7 +3,7 @@ from queue import Queue
 
 DAY = 21
 
-encode = {'.': 0, '#': -2, 'S': 1}
+encode = {'.': 0, '#': -2, 'S': 0}
 di = (0, 0, 1, -1)
 dj = (1, -1, 0, 0)
 
@@ -22,37 +22,63 @@ def Lee(map: list[list], start: tuple[int], max_step: int) -> None:
         for d in range(4):
             i_next, j_next = i+di[d], j+dj[d]
 
-            if is_ok(i_next, j_next, map) and map[i_next][j_next] == encode['.'] and map[i][j] < max_step:
+            if is_ok(i_next, j_next, map) and map[i_next][j_next] == encode['.'] and map[i][j] < max_step+1:
                 map[i_next][j_next] = map[i][j] + 1
                 Q.put((i_next, j_next))
 
 
-def part1(inp: list[str]) -> int:
-    mat = []
-    for line in inp:
-        mat.append([encode[x] for x in line])
-    step = 64
-
+def solve(mat: list[list], step: int) -> int:
     start = (len(mat)//2, len(mat)//2)
     
     Lee(mat, start, step)
-
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            if mat[i][j] == step:
-                
-                for d in range(4):
-                    i_next, j_next = i+di[d], j+dj[d]
-
-                    if is_ok(i_next, j_next, mat) and mat[i_next][j_next] == encode['.']:
-                        mat[i_next][j_next] = -3
     
     mat = np.array(mat)
-    return np.sum(mat%2 == 1)
+    return np.sum(((mat%2 != step%2) & (mat > 0)))
+
+
+def get_mat_of_scale(inp: list[str], scale: int) -> list[list]:
+    mat = []
+    for line in inp:
+        mat.append([encode[x] for x in line])
+    
+    mat = np.tile(np.array(mat), (2*scale-1, 2*scale-1)).tolist()
+    mat[len(mat)//2][len(mat)//2] = 1
+    return mat
+
+
+def part1(inp: list[str]) -> int:
+    return solve(get_mat_of_scale(inp, 1), 64)
 
 
 def part2(inp: list[str]) -> int:
-    return 0
+    step = 26501365
+    n = len(inp)
+
+    xs = [n//2 + i*n for i in range(3)]
+    ys = [solve(get_mat_of_scale(inp, i+1), x) for i, x in enumerate(xs)]
+    
+    # a*0^2 + b*0 + c = y0 => c = y0
+    # a*1^2 + b*1 + c = y1 => a + b + c = y1 => a + b = y1 - c
+    # a*2^2 + b*2 + c = y2 => 4a + 2b + c = y2 => 4a + 2b = y2 - c
+
+    # a + b = y1 - c
+    # 4a + 2b = y2 - c
+
+    # b = y1 - c - a
+    # 4a + 2(y1 - c - a) = y2 - c
+    # 4a + 2y1 - 2c - 2a = y2 - c
+    # 2a = y2 - c - 2y1 + 2c
+    # 2a = y2 + c - 2y1
+    # a = (y2 + c - 2y1) / 2
+    
+    c = int(ys[0])
+    a = int((ys[2] + c - 2*ys[1]) // 2)
+    b = int(ys[1] - c - a)
+
+    # step = n//2 + i * n
+    i = (step-n//2) // n
+    
+    return (a * (i**2)) + (b * i) + c
 
 
 def read_input_file(filename: str) -> list[str]:
